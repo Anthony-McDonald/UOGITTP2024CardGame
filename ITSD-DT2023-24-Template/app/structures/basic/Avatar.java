dec4c40d78cc771f5519ce6ca7ac0739f5a60789
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-17 15:38:01 +0000

    changed Avatar Units to have different ID numbers

--- ITSD-DT2023-24-Template/app/structures/basic/Avatar.java
+++ ITSD-DT2023-24-Template/app/structures/basic/Avatar.java
@@ -27,9 +27,9 @@ public class Avatar implements MoveableUnit {
 		this.lastTurnMoved = 0;
 		this.userOwned = player.isUserOwned();
 		if (this.userOwned){ //if human
-			this.unit = BasicObjectBuilders.loadUnit(StaticConfFiles.humanAvatar, 0, Unit.class);
+			this.unit = BasicObjectBuilders.loadUnit(StaticConfFiles.humanAvatar, 1000, Unit.class);
 		}else { //if AI
-			this.unit = BasicObjectBuilders.loadUnit(StaticConfFiles.aiAvatar, 0, Unit.class);
+			this.unit = BasicObjectBuilders.loadUnit(StaticConfFiles.aiAvatar, 1001, Unit.class);
 		}
 	}
 
