82d1c6e41974615313946630d5ffb7e92c4ea768
Tin Lok Liu <lokliu@hotmail.co.uk>
2024-02-20 16:46:22 +0000

    Created OpeningGambit interface & added them to appropriate classes

--- ITSD-DT2023-24-Template/app/allCards/GloomChaser.java
+++ ITSD-DT2023-24-Template/app/allCards/GloomChaser.java
@@ -3,11 +3,18 @@ package allCards;
 import structures.basic.BigCard;
 import structures.basic.Creature;
 import structures.basic.MiniCard;
+import structures.basic.OpeningGambit;
 import structures.basic.Unit;
 
-public class GloomChaser extends Creature{
+public class GloomChaser extends Creature implements OpeningGambit{
 
     public GloomChaser(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard, boolean isCreature, String unitConfig, int maxHealth, int currentHealth, int attack, int turnSummoned, int lastTurnMoved, Unit unit, boolean userOwned) {
         super(id, cardname, manacost, miniCard, bigCard, isCreature, unitConfig, maxHealth, currentHealth, attack, turnSummoned, lastTurnMoved,unit, userOwned);
     }
+
+	@Override
+	public void openingGambit() {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ ITSD-DT2023-24-Template/app/structures/basic/OpeningGambit.java
@@ -0,0 +1,12 @@
+package structures.basic;
+
+public interface OpeningGambit {
+
+/*
+ Units that have openingGambit are
+ Gloom Chaser
+ Nightsorrow Assassin
+ Silverguard Squire
+ */
+	public void openingGambit();
+}
