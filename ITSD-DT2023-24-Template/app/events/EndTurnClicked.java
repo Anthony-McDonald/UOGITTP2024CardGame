8bfe8d8604ea8b223793dcb57fd1baa45afd126a
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-02-20 21:35:15 +0000

    EndTurnClicked

--- ITSD-DT2023-24-Template/app/events/EndTurnClicked.java
+++ ITSD-DT2023-24-Template/app/events/EndTurnClicked.java
@@ -1,11 +1,11 @@
 package events;
 
-import actors.GameActor;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import akka.actor.ActorRef;
 import commands.BasicCommands;
 import structures.GameState;
+import structures.basic.AI;
 import structures.basic.Player;
 
 /**
@@ -24,18 +24,19 @@ public class EndTurnClicked implements EventProcessor{
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
 		//remove player 1's unspent mana
+		gameState.getPlayer1().setMana(0, out);
 		//insert AI method where AI makes moves
+		//AI artificial = (AI) gameState.getPlayer2(); 
+		//artificial.aiMoved(out,gameState.getPlayer2().);
 		//remove player 2's unspent mana
+		gameState.getPlayer2().setMana(0, out);
 		//insert draw card for each player
+		gameState.getPlayer1().drawCard();
+		gameState.getPlayer2().drawCard();
 		gameState.setTurnNumber(gameState.getTurnNumber()+1); //increase turnNumber
-		Player player1 = GameActor.gameState.getPlayer1();
-//		System.out.println("---");
-//		System.out.println(player1.getHand());
-		player1.drawCard();
-//		System.out.println(player1.getHand());
-//		System.out.println(GameActor.out);
-
 		//increase both players mana by (turn number+1)
+		gameState.getPlayer1().setMana(gameState.getTurnNumber()+1, out);
+		gameState.getPlayer2().setMana(gameState.getTurnNumber()+1, out);
 		String nextTurnMessage = "It's now turn " + gameState.getTurnNumber() + "! Make your moves.";
 		BasicCommands.addPlayer1Notification(out, nextTurnMessage, 2 );
 		gameState.setLastMessage(GameState.noEvent);
