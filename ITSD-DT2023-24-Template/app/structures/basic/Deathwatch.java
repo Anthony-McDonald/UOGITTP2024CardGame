926a333cb46adcbe3b810149a5ecf851161fc8e3
Tin Lok Liu <lokliu@hotmail.co.uk>
2024-02-22 20:44:11 +0000

    added logic to deathwatch for Bad Omen & Shadow Watcher

--- ITSD-DT2023-24-Template/app/structures/basic/Deathwatch.java
+++ ITSD-DT2023-24-Template/app/structures/basic/Deathwatch.java
@@ -2,4 +2,12 @@ package structures.basic;
 
 public interface Deathwatch {
     void deathWatch();
+    
+    /*
+     Units that have deathwatch are
+     Bad Omen
+     Shadow Watcher
+     Bloodmoon Priestess
+     Shadowdancer
+     */
 }
