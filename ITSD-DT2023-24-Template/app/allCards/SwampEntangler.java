0e6161e386f240ea93a8403f7b10b0510615de42
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-24 21:07:08 +0000

    SwampEntangler constructor

--- ITSD-DT2023-24-Template/app/allCards/SwampEntangler.java
+++ ITSD-DT2023-24-Template/app/allCards/SwampEntangler.java
@@ -9,6 +9,10 @@ import structures.basic.Unit;
 public class SwampEntangler extends Creature implements Provoke{
     public SwampEntangler(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard, boolean isCreature,  String unitConfig) {
         super(id, cardname, manacost, miniCard, bigCard, isCreature, unitConfig);
+        this.userOwned = false;
+        this.attack = 0;
+        this.currentHealth = 3;
+        this.maxHealth = currentHealth;
     }
 
 	@Override
