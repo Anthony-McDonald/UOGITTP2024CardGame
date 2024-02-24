cf0948d0ab16bc2a8180f4f0ac21f4e42dad3b5b
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-24 21:22:41 +0000

    YoungFlamewing constructor

--- ITSD-DT2023-24-Template/app/allCards/YoungFlamewing.java
+++ ITSD-DT2023-24-Template/app/allCards/YoungFlamewing.java
@@ -8,5 +8,9 @@ import structures.basic.Unit;
 public class YoungFlamewing extends Creature{
     public YoungFlamewing(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard, boolean isCreature,  String unitConfig) {
         super(id, cardname, manacost, miniCard, bigCard, isCreature, unitConfig);
+        this.userOwned = false;
+        this.attack = 5;
+        this.currentHealth = 4;
+        this.maxHealth = currentHealth;
     }
 }
