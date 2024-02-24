892dffc271a9f5ac823aa034b9b8ea1d61cdf4c7
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-24 21:18:44 +0000

    SaberspineTiger constructor

--- ITSD-DT2023-24-Template/app/allCards/SaberspineTiger.java
+++ ITSD-DT2023-24-Template/app/allCards/SaberspineTiger.java
@@ -8,5 +8,9 @@ import structures.basic.Unit;
 public class SaberspineTiger extends Creature{
     public SaberspineTiger(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard, boolean isCreature,  String unitConfig) {
         super(id, cardname, manacost, miniCard, bigCard, isCreature, unitConfig);
+        this.userOwned = false;
+        this.attack = 3;
+        this.currentHealth = 2;
+        this.maxHealth = currentHealth;
     }
 }
