b338ebe527458617c813e4087115c16eb51c5290
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-27 15:13:21 +0000

    added variable to AI to store all units

--- ITSD-DT2023-24-Template/app/structures/basic/AI.java
+++ ITSD-DT2023-24-Template/app/structures/basic/AI.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class AI extends Player  {
 	private GameState gameState;
 	private ActorRef actorRef;
+	private ArrayList<MoveableUnit> allUnits;
 
 	public AI(boolean userOwned, GameState gameState) {
 		super(userOwned);
