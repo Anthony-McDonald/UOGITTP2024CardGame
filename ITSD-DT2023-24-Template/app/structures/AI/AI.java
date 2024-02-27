8e8a8c0f6c35f21d936569d219c709bcc3101294
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-02-27 22:19:21 +0000

    added method for calculating distance between tiles, added method for returning list of all summonable tiles for AIUnitSummons

--- ITSD-DT2023-24-Template/app/structures/AI/AI.java
+++ ITSD-DT2023-24-Template/app/structures/AI/AI.java
@@ -3,9 +3,7 @@ import actors.GameActor;
 import akka.actor.ActorRef;
 import commands.BasicCommands;
 import structures.GameState;
-import structures.basic.Avatar;
-import structures.basic.MoveableUnit;
-import structures.basic.Player;
+import structures.basic.*;
 
 import java.util.ArrayList;
 
@@ -20,7 +18,82 @@ public class AI extends Player {
 
 	}
 
+	public static void main(String[] args) {
+		GameState gameState1 = new GameState();
+		Board board = gameState1.getBoard();
+		Tile tile1 = board.getTile(2, 3);
+		Tile tile2 = board.getTile(7, 3);
+		Wraithling wraithling1 = new Wraithling();
+		wraithling1.setUserOwned(false);
+		tile1.setUnit(wraithling1);
+		Wraithling wraithling2 = new Wraithling();
+		wraithling2.setUserOwned(false);
+		tile2.setUnit(wraithling2);
+		AI newAI = new AI(false, gameState1);
+		ArrayList<Tile> tiles = newAI.tilesForAIUnitSummons();
+		for (Tile tile: tiles){
+			System.out.println(tile.getTilex() + "," + tile.getTiley());
+		}
+
+
+	}
+
 	public void setActorRef(ActorRef out){
 		this.actorRef = out;
 	}
+
+	public void makeActions(){
+		while(this.hasActions()){
+
+		}
+	}
+
+	public boolean hasActions(){ //expand class as functionality increases
+		for (Card card:this.hand) {
+			if (card instanceof Creature && card.getManacost() <= this.mana) { //has enough mana for summoning
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static double calculateDistance(Tile startingTile, Tile targetTile){ //method for calculating distance between two tiles. multipurpose.
+		double xDistance = startingTile.getTilex() - targetTile.getTilex();
+		double yDistance = startingTile.getTiley() - targetTile.getTiley();
+		double distance = Math.sqrt((xDistance * xDistance) + (yDistance*yDistance)); //pythagorean theorem for calculating distance to tile;
+		return distance;
+
+	}
+
+	public void summonUnits(){
+		for (Card card : this.hand){
+
+		}
+	}
+
+	public ArrayList<Tile> tilesForAIUnitSummons(){
+		ArrayList<Tile> tilesForAIUnits = new ArrayList<>();
+		ArrayList<MoveableUnit>AIUnits = gameState.getBoard().friendlyUnits(false); //returns all AI units
+		for (MoveableUnit unit : AIUnits){
+			int tileX = unit.getTile().getTilex();
+			int tileY = unit.getTile().getTiley();
+			for (int i = tileX - 1; i<=tileX+1;i++){ //loop to search adjacent tiles to units
+				for (int j = tileY - 1; j<=tileY+1; j++){
+					if ( 0<=i && i<=8 && 0<=j && j<=4 ) { //if coord in board range
+						Tile possibleTile = gameState.getBoard().getTile(i,j);
+						if (possibleTile.getUnit()==null){ //no unit on tile
+							if (!tilesForAIUnits.contains(possibleTile)) { //if it doesn't contain the tile
+								tilesForAIUnits.add(possibleTile);
+							}
+						}
+					}
+				}
+			}
+		}
+		return tilesForAIUnits;
+	}
+
+
 }
+
---
+++ ITSD-DT2023-24-Template/test/AITest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import structures.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+public class AITest {
+
+}
