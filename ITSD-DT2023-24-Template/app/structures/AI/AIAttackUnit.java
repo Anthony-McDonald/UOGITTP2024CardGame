10acc25f5e0c753569bd02da9fe9224b754f67cb
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-03-02 16:52:52 +0000

    added action for performing unit attack

--- ITSD-DT2023-24-Template/app/structures/AI/AIAttackUnit.java
+++ ITSD-DT2023-24-Template/app/structures/AI/AIAttackUnit.java
@@ -1,21 +1,37 @@
 package structures.AI;
 
+import akka.actor.ActorRef;
 import structures.GameState;
 import structures.basic.MoveableUnit;
 import structures.basic.Tile;
 
 public class AIAttackUnit extends UnitAction{
     private MoveableUnit enemyUnit;
-    private int actionScore = 7; //adjust this to adjust starting weight
+
 
     public AIAttackUnit(MoveableUnit actionTaker, GameState gameState, MoveableUnit enemyUnit){
         this.actionTaker = actionTaker;
         this.gameState = gameState;
         this.enemyUnit = enemyUnit;
-        if (isAttackDangerous(enemyUnit)){
+        this.actionScore = 7; //adjust this to adjust starting weighting
+        this.assessScore();
 
+
+    }
+
+    @Override
+    public void makeAction(ActorRef out){
+        actionTaker.attackUnit(out, enemyUnit.getTile(), gameState);
+    }
+
+    public void assessScore(){
+        if (!isAttackDangerous(enemyUnit)){
+            //attack will kill enemy unit so keep actionscore as 7 (high weighting)
+        }else{
+            //attack will kill attacker so now assess value
+            //assess change to enemy health to adjust impact of health
+            this.setActionScore(1+actionTaker.getAttack()); //higher score based on damage done
         }
-
     }
 
 
