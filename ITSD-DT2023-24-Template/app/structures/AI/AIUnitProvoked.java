b2d7d1707c39934dbe19ab93b825d99c9085c52b
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-03-02 15:38:30 +0000

    added action for assessing weighting of provoke

--- ITSD-DT2023-24-Template/app/structures/AI/AIUnitProvoked.java
+++ ITSD-DT2023-24-Template/app/structures/AI/AIUnitProvoked.java
@@ -1,2 +1,46 @@
-package structures.AI;public class AIUnitProvoked {
+package structures.AI;
+
+import akka.actor.ActorRef;
+import structures.GameState;
+import structures.basic.MoveableUnit;
+
+public class AIUnitProvoked extends UnitAction {
+
+    private int actionScore = 1;
+    private MoveableUnit provoker;
+    public AIUnitProvoked(MoveableUnit actionTaker, GameState gameState, MoveableUnit provoker){
+        this.actionTaker = actionTaker;
+        this.gameState = gameState;
+        this.provoker = provoker;
+        if (this.IsAttackDangerous()){
+            actionScore = 0;
+
+        }
+
+    }
+
+    @Override
+    public void makeAction(ActorRef out){
+        if (!this.IsAttackDangerous()){
+            actionTaker.attackUnit(out, provoker.getTile(),gameState);
+        }
+    }
+
+    public boolean IsAttackDangerous(){
+        //method for assessing if attack on provoker will kill attacker
+        int provokerHealth = provoker.getCurrentHealth();
+        int actionTakerHealth = actionTaker.getCurrentHealth();
+        int provokerAttack = provoker.getAttack();
+        int actionTakerAttack = actionTaker.getAttack();
+        if((provokerHealth-actionTakerAttack)<=0){
+            //attack will kill provoker
+            return false;
+        }
+        if ((actionTakerHealth - provokerAttack)<=0){
+            return true;
+
+            //counter attack will kill attacker
+        }
+        return false;
+    }
 }
