b511b4134582a616275829db0d68bdf4e6796b3b
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2024-03-05 12:18:56 +0000

    made UnitAction abstract

--- ITSD-DT2023-24-Template/app/structures/AI/Actions/UnitAction.java
+++ ITSD-DT2023-24-Template/app/structures/AI/Actions/UnitAction.java
@@ -5,7 +5,7 @@ import structures.GameState;
 import structures.basic.MoveableUnit;
 import structures.basic.Tile;
 
-public class UnitAction {
+public abstract class UnitAction {
     protected Tile targetTile;
     protected boolean isActionPossible;
     protected MoveableUnit actionTaker;
