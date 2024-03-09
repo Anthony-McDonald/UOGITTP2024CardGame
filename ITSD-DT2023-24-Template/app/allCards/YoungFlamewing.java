a097824d78ba8c0a1a4e10e062a0d90a393be690
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-03-09 14:50:06 +0000

    YoungFlamewing javadocs

--- ITSD-DT2023-24-Template/app/allCards/YoungFlamewing.java
+++ ITSD-DT2023-24-Template/app/allCards/YoungFlamewing.java
@@ -4,7 +4,9 @@ import structures.basic.BigCard;
 import structures.basic.Creature;
 import structures.basic.MiniCard;
 import structures.basic.Unit;
-
+/**
+ * This is the class for Young Flamewing's implementation
+ */
 public class YoungFlamewing extends Creature{
     public YoungFlamewing(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard, boolean isCreature,  String unitConfig) {
         super(id, cardname, manacost, miniCard, bigCard, isCreature, unitConfig);
@@ -13,6 +15,10 @@ public class YoungFlamewing extends Creature{
         this.currentHealth = 4;
         this.maxHealth = currentHealth;
     }
-  
-    //has Fly ability.
+    /**
+     * This card has fly ability, 
+     * which allows Young Flamewing to move to any unoccupied space on the board
+     * The canMove() method handles fly logic from UnitCommands 
+     */
+
 }
