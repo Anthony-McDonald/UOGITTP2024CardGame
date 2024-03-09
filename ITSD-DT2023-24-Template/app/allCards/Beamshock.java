4bb43e4ccdf23d9b3479b2f23b719c34b1dbaa3a
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-03-09 13:10:47 +0000

    Beamshock javadocs

--- ITSD-DT2023-24-Template/app/allCards/Beamshock.java
+++ ITSD-DT2023-24-Template/app/allCards/Beamshock.java
@@ -6,13 +6,20 @@ import structures.GameState;
 import structures.basic.*;
 import utils.BasicObjectBuilders;
 import utils.StaticConfFiles;
-
+/**
+ * This is the class for BeamShock implementation
+ */
 public class Beamshock extends Spell{
 
     public Beamshock(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard, boolean isCreature,  String unitConfig) {
         super(id, cardname, manacost, miniCard, bigCard, isCreature, unitConfig);
     }
-
+    /**
+     * The spellEffect method stun the unit targeted and set the counter to 1
+     * @param tile
+     * @param out
+     * @param gameState
+     */ 
     public void spellEffect(Tile tile, ActorRef out, GameState gameState) {
         // Play the animation
         EffectAnimation effect = BasicObjectBuilders.loadEffect(StaticConfFiles.f1_martyrdom);
