0c3727fef107553c1b39766a76ca0da0987d9e2d
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-03-09 14:23:09 +0000

    ShadowWatcher javadocs

--- ITSD-DT2023-24-Template/app/allCards/ShadowWatcher.java
+++ ITSD-DT2023-24-Template/app/allCards/ShadowWatcher.java
@@ -7,7 +7,9 @@ import structures.basic.Creature;
 import structures.basic.Deathwatch;
 import structures.basic.MiniCard;
 import structures.basic.Unit;
-
+/**
+* This is the class for Shadow Watcher's implementation
+*/
 public class ShadowWatcher extends Creature implements Deathwatch{
 
     public ShadowWatcher(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard, boolean isCreature,  String unitConfig) {
@@ -18,8 +20,12 @@ public class ShadowWatcher extends Creature implements Deathwatch{
 		this.maxHealth = currentHealth;
     }
 
+    /**
+     * The deathWatch method set the Shadow Watcher +1 attack +1 hp permanently (max health)
+     * @param out
+     * @param gameState
+     */
     
-    //gains +1 attack +1 hp permanently (max health)
 	@Override
 	public void deathWatch(ActorRef out, GameState gameState) {
 		this.setAttack(getAttack()+1, out);
