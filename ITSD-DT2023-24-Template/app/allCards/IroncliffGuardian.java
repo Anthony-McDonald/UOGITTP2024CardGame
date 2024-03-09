4f6e5886a784180f770c48bd29406bbb84b716e9
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-03-09 13:58:48 +0000

    Ironcliff Guardian javadocs

--- ITSD-DT2023-24-Template/app/allCards/IroncliffGuardian.java
+++ ITSD-DT2023-24-Template/app/allCards/IroncliffGuardian.java
@@ -5,7 +5,9 @@ import structures.basic.Creature;
 import structures.basic.MiniCard;
 import structures.basic.Provoke;
 import structures.basic.Unit;
-
+/**
+ * This is the class for Ironcliff Guardian's implementation
+ */
 public class IroncliffGuardian extends Creature implements Provoke{
     public IroncliffGuardian(int id, String cardname, int manacost, MiniCard miniCard, BigCard bigCard, boolean isCreature,  String unitConfig) {
         super(id, cardname, manacost, miniCard, bigCard, isCreature, unitConfig);
@@ -14,7 +16,9 @@ public class IroncliffGuardian extends Creature implements Provoke{
         this.currentHealth = 10;
         this.maxHealth = currentHealth;
     }
-
+    /**
+     * The provoke set enemy unit only be able to attack Ironcliff Guardian 
+     */
 	@Override
 	public void provoke() {
 		// TODO Auto-generated method stub
