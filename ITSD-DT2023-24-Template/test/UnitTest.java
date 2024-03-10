bc62a34afd365bf50820374c193dc6653730ad1e
Wang, Ruxuan <2745883W@student.gla.ac.uk>
2024-03-10 01:41:49 +0000

    add UnitTest.java

---
+++ ITSD-DT2023-24-Template/test/UnitTest.java
@@ -0,0 +1,32 @@
+// This class tests the Unit class to make sure it works correctly.
+public class UnitTest {
+
+    private Unit unit;
+
+  
+    public void setUp() {
+
+    }
+
+
+    @Test
+    public void testHealth() {
+    }
+
+    @Test
+    public void testAttack() {
+
+    }
+
+
+    @Test
+    public void testMove() {
+
+    }
+
+
+    @Test
+    public void testAttackMethod() {
+        // Test if the unit attacks correctly.
+
+    // Add more test methods maybe.
